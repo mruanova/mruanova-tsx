3cd415ca9442f71218708eb89df26c5395b63269
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2020-05-01 19:25:30 -0500

    d3-box

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,14 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+// import d3 from 'd3';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+  },
+  appBar:{
+    backgroundColor:'gray'
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -27,7 +31,7 @@ const App: React.FC = () => {
   const classes = useStyles();
   return (
     <div className="App">
-      <AppBar position="static">
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
