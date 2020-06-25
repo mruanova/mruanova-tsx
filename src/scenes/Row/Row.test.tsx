6a00058fe989ecfe48c53a2f05d35e1ce7362cec
Mauricio Ruanova <mruanova@gmail.com>
2020-06-25 17:24:14 -0500

    unit-test

--- src/scenes/Row/Row.test.tsx
+++ src/scenes/Row/Row.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Row';
+import Row from './Row';
+import Project from '../../interfaces/Project';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Row />, div);
+  const detailsStyles: React.CSSProperties = { whiteSpace: 'pre-wrap' };
+  const data: Project[] = [];
+  ReactDOM.render(<Row index={1} style={detailsStyles} data={data} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
